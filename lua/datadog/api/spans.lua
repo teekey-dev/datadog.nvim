5103d0a354be022c3e8baf3b9a05edeebc1ef845
Taehwa Kang <taehwa.kang@remember.co.kr>
2026-04-02 22:07:26 +0900

    🐛 fix: add more detailed span response logging

--- lua/datadog/api/spans.lua
+++ lua/datadog/api/spans.lua
@@ -25,15 +25,18 @@ function M:search_by_issue(issue_id, from_iso, to_iso, callback)
 	}
 
 	self._api:_request("POST", "spans/events/search", request_body, function(response, err)
+		print("[Datadog Spans] Error: " .. vim.inspect(err))
+		print("[Datadog Spans] Response: " .. vim.inspect(response))
+		
 		if err then
 			callback(nil, err)
 			return
 		end
 		
-		print("[Datadog Spans] Response: " .. vim.json.encode(response))
-		
 		if response and response.data and #response.data > 0 then
-			callback(response.data[1], nil)
+			local span = response.data[1]
+			print("[Datadog Spans] Span: " .. vim.inspect(span))
+			callback(span, nil)
 		else
 			callback(nil, nil)
 		end
